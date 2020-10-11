2a56d33635c596ce4980a5cdd9370d8b7a97fc1f
Alexander <david_15vc@hotmail.com>
2020-10-12 00:56:32 +0200

    Creación de componentes principales de la parte front

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+<!--     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <MiComponente></MiComponente>
+ -->    
+    <HeaderComponent></HeaderComponent>
+    <SliderComponent></SliderComponent>
+    <div class="center">
+      <LastArticles></LastArticles>
+      <SidebarComponent></SidebarComponent>
+    </div>
+    <div class="clearfix"></div>
+    <FooterComponent></FooterComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import MiComponente from './components/MiComponente.vue';
+//import HelloWorld from './components/HelloWorld.vue';
+//import MiComponente from './components/MiComponente.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import SliderComponent from './components/SliderComponent.vue';
+import SidebarComponent from './components/SidebarComponent';
+import LastArticles from './components/LastArticles';
+import FooterComponent from './components/FooterComponent';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    MiComponente
+/*     HelloWorld,
+    MiComponente,
+ */    
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    LastArticles,
+    FooterComponent
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+
+@import './assets/css/style.css';
 </style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer id="footer">
+        <div class="center">
+            <p>
+                &copy; Master en Frameworks
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name:'FooterComponent'
+}
+</script>
